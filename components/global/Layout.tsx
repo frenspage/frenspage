d68ba5a1eafb5fa44179d6afcbe5c5d0f314de84
DanielWahl <contact@danielwahl.lu>
2021-12-17 12:20:35 +0100

    Added new favicon and head-title

--- components/global/Layout.tsx
+++ components/global/Layout.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Script from "next/script";
+import Head from "next/Head";
 
 interface Props {
     addClass?: string;
@@ -8,6 +9,11 @@ interface Props {
 const Layout: NextPage<Props> = ({ children, addClass }) => {
     return (
         <div className={"root " + addClass}>
+            <Head>
+                <title>frens.page</title>
+                <meta name="description" content="gm" />
+                <link rel="shortcut icon" href="/favicon.gif" />
+            </Head>
             {children}
             <Script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js" />
         </div>
