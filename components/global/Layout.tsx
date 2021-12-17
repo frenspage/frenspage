01d628fd63c9431165e2bba91d2c0846a5e8d12b
DanielWahl <contact@danielwahl.lu>
2021-12-17 12:39:10 +0100

    Fixed icon problems

--- components/global/Layout.tsx
+++ components/global/Layout.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Script from "next/script";
-import Head from "next/head";
 
 interface Props {
     addClass?: string;
@@ -9,11 +8,6 @@ interface Props {
 const Layout: NextPage<Props> = ({ children, addClass }) => {
     return (
         <div className={"root " + addClass}>
-            <Head>
-                <title>frens.page</title>
-                <meta name="description" content="gm" />
-                <link rel="shortcut icon" href="/favicon.gif" />
-            </Head>
             {children}
             <Script src="https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js" />
         </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,19 @@ import type { AppProps } from "next/app";
 import "../styles/main.sass";
 import { MoralisProvider } from "react-moralis";
 import { PopupProvider } from "../context/PopupContext";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
     const appId: string = process.env.NEXT_PUBLIC_APPID ?? "";
     const serverUrl: string = process.env.NEXT_PUBLIC_SERVERURL ?? "";
     return (
-  
         <MoralisProvider appId={appId} serverUrl={serverUrl}>
             <PopupProvider>
+                <Head>
+                    <title>frens.page</title>
+                    <meta name="description" content="gm" />
+                    <link rel="icon" href="/images/favicon.png" />
+                </Head>
                 <Component {...pageProps} />
             </PopupProvider>
         </MoralisProvider>
